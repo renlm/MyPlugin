277e17d3db94b539ccb4e03aa94ad781b6e632a2
任黎明 <renlm-a@glodon.com>
2020-12-25 16:16:47 +0800

    s

--- MyExcel/src/test/java/cn/renlm/plugins/DemoTest.java
+++ MyExcel/src/test/java/cn/renlm/plugins/DemoTest.java
@@ -1,15 +1,12 @@
 package cn.renlm.plugins;
 
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
 
-import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.lang.Console;
 import lombok.SneakyThrows;
 
 /**
@@ -20,58 +17,16 @@ import lombok.SneakyThrows;
  */
 public class DemoTest {
 
+	/**
+	 * 生成模板
+	 */
 	@Test
 	@SneakyThrows
-	public void test() {
-		String config = "MyExcelTest.xml";
-		String path = "C:/Users/renlm-a/Desktop/导出测试.xlsx";
+	public void createTemplate() {
+		String path = FileUtil.getUserHomePath() + "/Desktop/Demo.xlsx";
 		FileUtil.del(path);
 		OutputStream stream = new FileOutputStream(path);
-
-		// 合并三个表格到指定模板
-		Workbook workbook = MyExcelUtil.createWorkbook(config, false, sheet1 -> {
-			String sheetName = sheet1.getSheetName();
-			InputStream in = FileUtil.getInputStream("MyExcelTest/1.扶贫对象-贫困户.xls");
-			MyExcelUtil.readBySax(config, in, sheetName, (data, checkResult) -> {
-				if (checkResult.isError()) {
-					if (checkResult.isProcess()) {
-						Console.log(checkResult);
-					} else { // 模板表头校验失败
-						Console.error(checkResult);
-					}
-				} else {
-					sheet1.write(CollUtil.newArrayList(data));
-				}
-			});
-		}, sheet2 -> {
-			String sheetName = sheet2.getSheetName();
-			InputStream in = FileUtil.getInputStream("MyExcelTest/2.帮扶措施-受益项目列表.csv");
-			MyExcelUtil.readBySax(config, in, sheetName, (data, checkResult) -> {
-				if (checkResult.isError()) {
-					if (checkResult.isProcess()) {
-						Console.log(checkResult);
-					} else { // 模板表头校验失败
-						Console.error(checkResult);
-					}
-				} else {
-					sheet2.write(CollUtil.newArrayList(data));
-				}
-			});
-		}, sheet3 -> {
-			String sheetName = sheet3.getSheetName();
-			InputStream in = FileUtil.getInputStream("MyExcelTest/3.农业局-种养殖.xlsx");
-			MyExcelUtil.readBySax(config, in, sheetName, (data, checkResult) -> {
-				if (checkResult.isError()) {
-					if (checkResult.isProcess()) {
-						Console.log(checkResult);
-					} else { // 模板表头校验失败
-						Console.error(checkResult);
-					}
-				} else {
-					sheet3.write(CollUtil.newArrayList(data));
-				}
-			});
-		});
+		Workbook workbook = MyExcelUtil.createWorkbook("Demo.xml", true);
 		workbook.write(stream);
 	}
 }
