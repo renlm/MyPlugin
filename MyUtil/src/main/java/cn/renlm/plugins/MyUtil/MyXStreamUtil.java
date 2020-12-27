98566103b0cc262cb8c30109044509dc68b6c1d5
任黎明 <renlm-a@glodon.com>
2020-12-27 19:34:57 +0800

    自定义工具集

---
+++ MyUtil/src/main/java/cn/renlm/plugins/MyUtil/MyXStreamUtil.java
@@ -0,0 +1,47 @@
+package cn.renlm.plugins.MyUtil;
+
+import java.io.InputStream;
+
+import com.thoughtworks.xstream.XStream;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Xml配置读取工具
+ * 
+ * @author renlm
+ *
+ */
+@UtilityClass
+public final class MyXStreamUtil {
+
+	/**
+	 * 读取
+	 * 
+	 * @param type
+	 * @param resource
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static final <C> C read(final Class<C> type, String resource) {
+		InputStream in = MyXStreamUtil.class.getClassLoader().getResourceAsStream(resource);
+		XStream xstream = create(type, in);
+		return (C) xstream.fromXML(in);
+	}
+
+	/**
+	 * 新建
+	 * 
+	 * @param type
+	 * @param in
+	 * @return
+	 */
+	private static final XStream create(final Class<?> type, InputStream in) {
+		XStream xstream = new XStream();
+		XStream.setupDefaultSecurity(xstream);
+		xstream.processAnnotations(type);
+		xstream.allowTypeHierarchy(type);
+		xstream.ignoreUnknownElements();
+		return xstream;
+	}
+}
