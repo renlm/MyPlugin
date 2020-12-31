f39dc7275e0d8e2a6ff539e41bfad762707f93b8
Renlm <renlmer@163.com>
2020-12-31 15:03:02 +0800

    代码调整

--- My-Plugin/src/main/java/cn/renlm/plugins/MyPlugin.java
+++ My-Plugin/src/main/java/cn/renlm/plugins/MyPlugin.java
@@ -16,7 +16,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MyPlugin {
 
-	public static final String AUTHOR = "renlm";
+	public static final String AUTHOR = "Renlm";
 
 	public static final String VERSION = "1.0.0";
 
