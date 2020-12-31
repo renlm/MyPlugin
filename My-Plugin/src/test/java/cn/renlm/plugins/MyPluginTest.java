e102b57f26485f4473c5f9ab6ee9c576bea8ef1e
Renlm <renlmer@163.com>
2020-12-31 12:24:16 +0800

    代码调整

---
+++ My-Plugin/src/test/java/cn/renlm/plugins/MyPluginTest.java
@@ -0,0 +1,17 @@
+package cn.renlm.plugins;
+
+import org.junit.Test;
+
+/**
+ * 插件打印
+ * 
+ * @author renlm
+ *
+ */
+public class MyPluginTest {
+
+	@Test
+	public void test() {
+		MyPlugin.printAllUtils();
+	}
+}
