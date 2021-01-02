e38dd7f7d8b519e7bfabe8c8bc7d66784c9b51eb
Renlm <renlmer@163.com>
2021-01-02 17:52:32 +0800

    修改

--- My-Plugin/src/main/java/cn/renlm/plugins/MyPlugin.java
+++ My-Plugin/src/main/java/cn/renlm/plugins/MyPlugin.java
@@ -18,7 +18,7 @@ public class MyPlugin {
 
 	public static final String AUTHOR = "Renlm";
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1.RELEASE";
 
 	/**
 	 * 显示所有插件类
