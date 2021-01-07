90db379049c3758801f63476f3369b5ef144d4df
Renlm <renlmer@163.com>
2021-01-07 14:53:01 +0800

    修改

--- MyCrawler/src/main/java/cn/renlm/plugins/MyCrawler/FontDecryption.java
+++ MyCrawler/src/main/java/cn/renlm/plugins/MyCrawler/FontDecryption.java
@@ -3,31 +3,25 @@ package cn.renlm.plugins.MyCrawler;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.fontbox.ttf.CmapLookup;
 import org.apache.fontbox.ttf.TTFParser;
 import org.apache.fontbox.ttf.TrueTypeFont;
 
-import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.lang.Dict;
+import cn.hutool.core.util.ReUtil;
 import cn.hutool.http.HttpUtil;
 import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
 
 /**
- * 字体工具
+ * 字体解密
  * 
- * @author 任黎明
+ * @author Renlm
  *
  */
+@UtilityClass
 public class FontDecryption {
-	public final static String split_GlyphCode = "@^|^@";
-	public final static String regex_split_GlyphCode = "@\\^\\|\\^@";
-	public final static String glyph_prefix = "&#";
-	public final static String glyph_suffix = ";";
-	public final static String regex_GlyphCode = glyph_prefix + "\\d{6}" + glyph_suffix;
 
 	/**
 	 * 获取字典
@@ -36,70 +30,27 @@ public class FontDecryption {
 	 * @return
 	 */
 	@SneakyThrows
-	public static CmapLookup getUnicodeCmapLookupFromTTF(String fonturl) {
+	public static final CmapLookup getUnicodeCmapLookupFromTTF(String fonturl) {
 		InputStream in = new ByteArrayInputStream(HttpUtil.downloadBytes(fonturl));
 		TrueTypeFont font = new TTFParser().parse(in);
 		return font.getUnicodeCmapLookup();
 	}
 
 	/**
-	 * 加密网页字体编码
+	 * 字体解密
 	 * 
-	 * @param html
-	 */
-	public static String safeGlyphCode(String html) {
-		Pattern pattern = Pattern.compile(regex_GlyphCode);
-		Matcher matcher = pattern.matcher(html);
-		while (matcher.find()) {
-			String base64 = Base64.encodeBase64URLSafeString(matcher.group().getBytes());
-			html = html.replaceAll(matcher.group(), split_GlyphCode + base64 + split_GlyphCode);
-		}
-		return html;
-	}
-
-	/**
-	 * 从加密网页字体编码中获取字体解码
-	 * 
-	 * @param _gmap         编号字符映射
-	 * @param cmap          编码映射
-	 * @param safeGlyphCode 加密网页字体编码
+	 * @param gmap       编号字符映射
+	 * @param cmap       编码字典
+	 * @param glyphCodes 加密字符串
 	 * @return
 	 */
-	public static String fetchDataFromSafeGlyphCode(Map<Integer, String> _gmap, CmapLookup cmap, String safeGlyphCode) {
-		if (StrUtil.isNotBlank(safeGlyphCode)) {
-			String glyphCode = "";
-			String[] arrs = safeGlyphCode.split(regex_split_GlyphCode);
-			for (String base64 : arrs) {
-				if (StrUtil.isNotBlank(base64)) {
-					String character = new String(Base64.decodeBase64(base64));
-					Integer glyphId = cmap.getGlyphId(Integer.valueOf(character.substring(2, character.length() - 1)));
-					glyphCode += _gmap.get(glyphId);
-				}
-			}
-			return glyphCode;
+	public static final String fetchFromGlyphCode(Dict gmap, CmapLookup cmap, String glyphCodes) {
+		StringBuffer buff = new StringBuffer();
+		List<String> codes = ReUtil.findAll("&#(\\d{6});", glyphCodes, 1);
+		for (String code : codes) {
+			int glyphId = cmap.getGlyphId(Integer.valueOf(code));
+			buff.append(gmap.get(String.valueOf(glyphId)));
 		}
-		return null;
-	}
-
-	/**
-	 * 转换明文为字体编码
-	 * 
-	 * @param gmap  字符编号映射
-	 * @param cmap  编码映射
-	 * @param plain 明文文本
-	 * @return
-	 */
-	public static String toGlyphCode(Map<String, Integer> gmap, CmapLookup cmap, String plain) {
-		String glyphCode = "";
-		if (StrUtil.isNotBlank(plain)) {
-			for (int i = 0; i < plain.length(); i++) {
-				int glyphId = gmap.get(String.valueOf(plain.charAt(i)));
-				List<Integer> charCodes = cmap.getCharCodes(glyphId);
-				for (Integer charCode : charCodes) {
-					glyphCode += glyph_prefix + charCode + glyph_suffix;
-				}
-			}
-		}
-		return glyphCode;
+		return buff.toString();
 	}
 }
--- MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -1,17 +1,21 @@
 package cn.renlm.plugins;
 
-import java.io.Serializable;
-
+import org.apache.fontbox.ttf.CmapLookup;
+import org.jsoup.nodes.Element;
 import org.junit.Test;
 
 import cn.edu.hfut.dmic.webcollector.fetcher.Visitor;
 import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
 import cn.edu.hfut.dmic.webcollector.model.Page;
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.lang.Dict;
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.http.HtmlUtil;
+import cn.hutool.json.JSONUtil;
 import cn.renlm.plugins.MyCrawler.CrawlerRequester;
-import lombok.Data;
+import cn.renlm.plugins.MyCrawler.FontDecryption;
 import lombok.SneakyThrows;
-import lombok.experimental.Accessors;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * 爬虫测试
@@ -19,14 +23,18 @@ import lombok.extern.slf4j.Slf4j;
  * @author Renlm
  *
  */
-@Slf4j
 public class MyCrawlerTest {
+	
+	String regexTtf = "https://((?!(https://))[\\s\\S])*\\.ttf";
+
+	Dict gmap = JSONUtil.toBean(ResourceUtil.readUtf8Str("GlyphCode.json"), Dict.class);
 
 	@Test
 	@SneakyThrows
 	public void test() {
 		CrawlerRequester requester = new CrawlerRequester();
 		MyCrawlerUtil.createDefault("crawl", true, crawler -> {
+			crawler.setThreads(1);
 			crawler.addSeed("https://www.qidian.com");
 			crawler.addRegex("https://book.qidian.com/info/.*");
 			crawler.setRequester(requester);
@@ -34,43 +42,25 @@ public class MyCrawlerTest {
 				@Override
 				public void visit(Page page, CrawlDatums next) {
 					if (page.matchUrl("https://book.qidian.com/info/.*")) {
-						log.info(page.url());
+						Element el = page.select("body>div.wrap>div.book-detail-wrap>div.book-information").first();
+						// 封面
+						String cover = el.select("div.book-img>a>img").attr("src");
+						// 书名
+						String name = el.select("div.book-info>h1>em").text();
+						// 作者
+						String author = el.select("div.book-info>h1>span>a").text();
+						// 简介
+						String intro = el.select("div.book-info>p.intro").text();
+						// 字数
+						Element wdEl = el.select("div.book-info>p>em").first();
+						String ttf = ReUtil.getGroup0(regexTtf, wdEl.select("style").html());
+						CmapLookup cmap = FontDecryption.getUnicodeCmapLookupFromTTF(ttf);
+						String glyphCodes = HtmlUtil.unescape(wdEl.select("span").text());
+						String wordNumber = FontDecryption.fetchFromGlyphCode(gmap, cmap, glyphCodes);
+						Console.log(cover, name, author, intro, ttf, wordNumber);
 					}
 				}
 			});
 		}).start(4);
-	}
-
-	@Data
-	@Accessors(chain = true)
-	public class Book implements Serializable {
-
-		private static final long serialVersionUID = 1L;
-
-		/**
-		 * 书名
-		 */
-		private String name;
-
-		/**
-		 * 作者
-		 */
-		private String author;
-
-		/**
-		 * 封面
-		 */
-		private String coverPhoto;
-
-		/**
-		 * 字数（万字）
-		 */
-		private Integer wordNumber;
-
-		/**
-		 * 简介
-		 */
-		private String briefIntroduction;
-
 	}
 }
