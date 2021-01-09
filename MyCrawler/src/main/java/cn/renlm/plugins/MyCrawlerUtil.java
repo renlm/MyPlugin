ea5f02d9f76637d125b8716e480b207392b1dcb8
Renlm <renlmer@163.com>
2021-01-09 21:54:37 +0800

    修改

--- MyCrawler/src/main/java/cn/renlm/plugins/MyCrawlerUtil.java
+++ MyCrawler/src/main/java/cn/renlm/plugins/MyCrawlerUtil.java
@@ -1,9 +1,5 @@
 package cn.renlm.plugins;
 
-import java.util.function.Consumer;
-
-import cn.edu.hfut.dmic.webcollector.crawler.Crawler;
-import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import lombok.experimental.UtilityClass;
 
 /**
@@ -15,27 +11,4 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MyCrawlerUtil {
 
-	private static final int threads = 5;
-
-	private static final int topN = Integer.MAX_VALUE;
-
-	private static final int maxExecuteCount = 3;
-
-	private static final int executeInterval = 1000;
-
-	/**
-	 * 默认配置
-	 * 
-	 * @param breadthCrawler
-	 * @param crawler
-	 * @return
-	 */
-	public Crawler defaultBreadthCrawler(BreadthCrawler breadthCrawler, Consumer<BreadthCrawler> crawler) {
-		breadthCrawler.setThreads(threads);
-		breadthCrawler.getConf().setTopN(topN);
-		breadthCrawler.getConf().setMaxExecuteCount(maxExecuteCount);
-		breadthCrawler.getConf().setExecuteInterval(executeInterval);
-		crawler.accept(breadthCrawler);
-		return breadthCrawler;
-	}
 }
--- MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -1,24 +1,13 @@
 package cn.renlm.plugins;
 
-import org.apache.fontbox.ttf.CmapLookup;
-import org.jsoup.nodes.Element;
 import org.junit.Test;
 
-import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
-import cn.edu.hfut.dmic.webcollector.model.Page;
-import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import cn.hutool.core.io.resource.ResourceUtil;
-import cn.hutool.core.lang.Console;
 import cn.hutool.core.lang.Dict;
-import cn.hutool.core.util.ReUtil;
-import cn.hutool.http.HtmlUtil;
 import cn.hutool.json.JSONUtil;
-import cn.renlm.plugins.MyCrawler.CrawlerRequester;
-import cn.renlm.plugins.MyCrawler.FontDecryption;
-import lombok.SneakyThrows;
 
 /**
- * 爬虫测试
+ * 爬虫
  * 
  * @author Renlm
  *
@@ -30,44 +19,7 @@ public class MyCrawlerTest {
 	Dict GMAP = JSONUtil.toBean(ResourceUtil.readUtf8Str("GlyphCode.json"), Dict.class);
 
 	@Test
-	@SneakyThrows
 	public void test() {
-		// 启动爬虫
-		CrawlerRequester requester = new CrawlerRequester();
-		MyCrawlerUtil.defaultBreadthCrawler(new BreadthCrawler("crawl", true) {
-			@Override
-			public void visit(Page page, CrawlDatums next) {
-				// 避免加密字体转义
-				page.html(ReUtil.replaceAll(page.html(), FontDecryption.REGEX, matcher -> {
-					return HtmlUtil.escape(matcher.group());
-				}));
-				// 书籍详情页
-				if (page.matchUrl("https://book.qidian.com/info/.*")) {
-					Element el = page.select("body>div.wrap>div.book-detail-wrap>div.book-information").first();
-					// 封面
-					String cover = el.select("div.book-img>a>img").attr("src");
-					// 书名
-					String name = el.select("div.book-info>h1>em").text();
-					// 作者
-					String author = el.select("div.book-info>h1>span>a").text();
-					// 简介
-					String intro = el.select("div.book-info>p.intro").text();
-					// 字数
-					Element wordNumberEl = el.select("div.book-info>p>em").first();
-					String ttf = ReUtil.getGroup0(REGEX_TTF, wordNumberEl.select("style").html());
-					CmapLookup cmap = FontDecryption.getUnicodeCmapLookupFromTTF(ttf);
-					String glyphCodes = HtmlUtil.unescape(wordNumberEl.select("span").html());
-					Double wordNumber = Double.valueOf(FontDecryption.fetchFromGlyphCode(GMAP, cmap, glyphCodes));
-					String wordNumberUnit = el.select("div.book-info>p>cite").first().text();
-					// 打印结果
-					Console.log(cover, name, author, intro, wordNumber + wordNumberUnit);
-				}
-			}
-		}, crawler -> {
-			crawler.setThreads(1);
-			crawler.addSeed("https://www.qidian.com");
-			crawler.addRegex("https://book.qidian.com/info/.*");
-			crawler.setRequester(requester);
-		}).start(2);
+
 	}
 }
