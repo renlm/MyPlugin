37d6a846a51d84fd2a604b902dc800bc571a598e
Renlm <renlmer@163.com>
2021-01-10 17:17:41 +0800

    修改

--- MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -2,7 +2,10 @@ package cn.renlm.plugins;
 
 import org.junit.Test;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.util.ReUtil;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.selector.Html;
 
 /**
  * 爬虫
@@ -15,9 +18,21 @@ public class MyCrawlerTest {
 	@Test
 	public void run() {
 		MyCrawlerUtil.createSpider(Site.me(), page -> {
-
+			String url = page.getUrl().get();
+			Html html = page.getHtml();
+			String regex = "(https://book.qidian.com/info/\\d+)";
+			page.addTargetRequests(html.links().regex(regex).all());
+			if (ReUtil.isMatch(regex, url)) {
+				page.putField("name", html.xpath("//div[@class='book-info']/h1/em/text()").get());
+				page.putField("author", html.xpath("//div[@class='book-info']/h1/span/a/text()").get());
+				page.putField("intro", html.xpath("//div[@class='book-info']/p[@class='intro']/text()").get());
+			} else {
+				page.setSkip(true);
+			}
 		}, (resultItems, task) -> {
-
-		}).run("");
+			if (!resultItems.isSkip()) {
+				Console.log(resultItems);
+			}
+		}).run("https://book.qidian.com");
 	}
 }
