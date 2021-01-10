53e04f88bafa0179ff2f7bb439ce7d9638a1c2c4
Renlm <renlmer@163.com>
2021-01-10 14:21:09 +0800

    修改

--- MyCrawler/src/main/java/cn/renlm/plugins/MyCrawlerUtil.java
+++ MyCrawler/src/main/java/cn/renlm/plugins/MyCrawlerUtil.java
@@ -1,9 +1,17 @@
 package cn.renlm.plugins;
 
+import java.util.function.Consumer;
+
 import lombok.experimental.UtilityClass;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.Pipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.scheduler.Scheduler;
 
 /**
- * 爬虫
+ * 爬虫工具
  * 
  * @author Renlm
  *
@@ -11,4 +19,35 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MyCrawlerUtil {
 
+	/**
+	 * 爬虫实例
+	 * 
+	 * @param pageProcessor
+	 * @param scheduler
+	 * @param pipeline
+	 * @param thread
+	 * @param urls
+	 * @return
+	 */
+	public static final Spider createSpider(PageProcessor pageProcessor, Scheduler scheduler, Pipeline pipeline,
+			int thread, String... urls) {
+		return Spider.create(pageProcessor).addUrl(urls).thread(thread).setScheduler(scheduler).addPipeline(pipeline);
+	}
+
+	/**
+	 * 缓存连接池
+	 * 
+	 * @param host
+	 * @param port
+	 * @param password
+	 * @param timeout
+	 * @param config
+	 * @return
+	 */
+	public static final JedisPool createJedisPool(String host, int port, String password, int timeout,
+			Consumer<JedisPoolConfig> config) {
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		config.accept(jedisPoolConfig);
+		return new JedisPool(jedisPoolConfig, host, port, timeout, password);
+	}
 }
--- MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -1,5 +1,10 @@
 package cn.renlm.plugins;
 
+import org.junit.Test;
+
+import cn.hutool.db.nosql.redis.RedisDS;
+import redis.clients.jedis.Jedis;
+
 /**
  * 爬虫
  * 
@@ -8,4 +13,11 @@ package cn.renlm.plugins;
  */
 public class MyCrawlerTest {
 
+	@Test
+	public void run() {
+		Jedis jedis = RedisDS.create().getJedis();
+		Long i = jedis.hset("jedis", "test", "false");
+		System.out.println(i);
+		System.out.println(jedis.hget("jedis", "test"));
+	}
 }
