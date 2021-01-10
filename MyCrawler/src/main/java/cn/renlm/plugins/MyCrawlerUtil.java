c39968b5c42d2bcbc743c0fa8ba3f7ecd1ba3ac0
Renlm <renlmer@163.com>
2021-01-10 13:19:55 +0800

    修改

---
+++ MyCrawler/src/main/java/cn/renlm/plugins/MyCrawlerUtil.java
@@ -0,0 +1,14 @@
+package cn.renlm.plugins;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * 爬虫
+ * 
+ * @author Renlm
+ *
+ */
+@UtilityClass
+public class MyCrawlerUtil {
+
+}
---
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -0,0 +1,11 @@
+package cn.renlm.plugins;
+
+/**
+ * 爬虫
+ * 
+ * @author Renlm
+ *
+ */
+public class MyCrawlerTest {
+
+}
