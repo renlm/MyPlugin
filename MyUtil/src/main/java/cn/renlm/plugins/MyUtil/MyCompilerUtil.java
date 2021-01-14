67de569d69e02182a0ddf75c5f1a3cc7d97f42dd
Renlm <renlmer@163.com>
2021-01-14 17:00:34 +0800

    修改

---
+++ MyUtil/src/main/java/cn/renlm/plugins/MyUtil/MyCompilerUtil.java
@@ -0,0 +1,54 @@
+package cn.renlm.plugins.MyUtil;
+
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.core.util.ReflectUtil;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import net.openhft.compiler.CompilerUtils;
+
+/**
+ * 代码编译
+ * 
+ * @author Renlm
+ *
+ */
+@UtilityClass
+public class MyCompilerUtil {
+
+	/**
+	 * 从代码中实例对象
+	 * 
+	 * @param <T>
+	 * @param className
+	 * @param javaCode
+	 * @return
+	 */
+	@SneakyThrows
+	@SuppressWarnings("unchecked")
+	public <T> T loadFromJava(String javaCode) {
+		String className = fetchClassName(javaCode);
+		String hashJavaCode = addHashToPackage(javaCode);
+		Class<?> clazz = CompilerUtils.CACHED_COMPILER.loadFromJava(className, hashJavaCode);
+		return (T) ReflectUtil.newInstance(clazz);
+	}
+
+	/**
+	 * 从代码中提取类名
+	 * 
+	 * @param javaCode
+	 * @return
+	 */
+	public static String fetchClassName(String javaCode) {
+		return ReUtil.get("(?m)^\\s*public\\s+class\\s+(\\w+)\\b", javaCode, 1);
+	}
+
+	/**
+	 * 追加Hash路径
+	 * 
+	 * @param javaCode
+	 * @return
+	 */
+	public String addHashToPackage(String javaCode) {
+		return ReUtil.get("(?im)^\\s*package\\s+([^;]+);", javaCode, 1);
+	}
+}
