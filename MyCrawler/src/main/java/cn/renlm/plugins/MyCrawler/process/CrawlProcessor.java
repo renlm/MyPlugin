15429f62e935884ee0d53e0e7c1f707d891ac35a
Renlm <renlmer@163.com>
2021-01-14 20:03:29 +0800

    修改

--- MyCrawler/src/main/java/cn/renlm/plugins/MyCrawler/process/CrawlProcessor.java
+++ MyCrawler/src/main/java/cn/renlm/plugins/MyCrawler/process/CrawlProcessor.java
@@ -3,17 +3,12 @@ package cn.renlm.plugins.MyCrawler.process;
 import cn.renlm.plugins.MyCrawler.data.MyProcessPage;
 
 /**
- * 抓取器
+ * 数据抓取
  * 
  * @author Renlm
  *
  */
 public interface CrawlProcessor<T> {
-
-	/**
-	 * 标记
-	 */
-	public static final String flagKey = "flag";
 
 	/**
 	 * 处理过程
