5e25f4814482633344146c7cc379857cf75531ea
Renlm <renlmer@163.com>
2021-01-16 13:26:09 +0800

    修改

--- MyCrawler/src/main/java/cn/renlm/plugins/MyCrawler/data/MyProcessPipe.java
+++ MyCrawler/src/main/java/cn/renlm/plugins/MyCrawler/data/MyProcessPipe.java
@@ -3,7 +3,6 @@ package cn.renlm.plugins.MyCrawler.data;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
-import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.scheduler.component.DuplicateRemover;
@@ -14,26 +13,15 @@ import us.codecraft.webmagic.scheduler.component.DuplicateRemover;
  * @author Renlm
  *
  */
+@Getter
 @AllArgsConstructor
 @Accessors(chain = true, fluent = true)
 public class MyProcessPipe {
 
-	@Getter
 	private final Task task;
 
-	@Getter
 	private final ResultItems resultItems;
 
 	private final DuplicateRemover duplicatedRemover;
 
-	/**
-	 * 请求是否重复
-	 * 
-	 * @param request
-	 * @param task
-	 * @return
-	 */
-	public final boolean isDuplicate(Request request, Task task) {
-		return this.duplicatedRemover.isDuplicate(request, task);
-	}
 }
