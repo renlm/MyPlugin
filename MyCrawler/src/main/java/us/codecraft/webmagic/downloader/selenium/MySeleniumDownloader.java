928bfd9a0c0d9b57a12bdaac27427cb0e812a4da
Renlm <renlmer@163.com>
2021-07-27 20:29:00 +0800

    ss

--- MyCrawler/src/main/java/us/codecraft/webmagic/downloader/selenium/MySeleniumDownloader.java
+++ MyCrawler/src/main/java/us/codecraft/webmagic/downloader/selenium/MySeleniumDownloader.java
@@ -1,5 +1,9 @@
 package us.codecraft.webmagic.downloader.selenium;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
@@ -12,62 +16,26 @@ import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.downloader.Downloader;
-// import us.codecraft.webmagic.selector.Html;
 import us.codecraft.webmagic.selector.PlainText;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.Map;
-
 /**
- * 使用Selenium调用浏览器进行渲染。目前仅支持chrome。<br>
- * 需要下载Selenium driver支持。<br>
+ * 使用Selenium调用浏览器进行渲染
+ * 
+ * @author Renlm
  *
- * @author code4crafter@gmail.com <br>
- *         Date: 13-7-26 <br>
- *         Time: 下午1:37 <br>
  */
 public class MySeleniumDownloader implements Downloader, Closeable {
 
-	private volatile WebDriverPool webDriverPool;
+	private volatile MyWebDriverPool webDriverPool;
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-	private int sleepTime = 0;
+	private int sleepTime = 1;
 
 	private int poolSize = 1;
 
-	// private static final String DRIVER_PHANTOMJS = "phantomjs";
-
-	/**
-	 * 新建
-	 *
-	 * @param chromeDriverPath chromeDriverPath
-	 */
-	public MySeleniumDownloader(String chromeDriverPath) {
-		System.getProperties().setProperty("webdriver.chrome.driver",
-				chromeDriverPath);
-	}
-
-	/**
-	 * Constructor without any filed. Construct PhantomJS browser
-	 * 
-	 * @author bob.li.0718@gmail.com
-	 */
-	public MySeleniumDownloader() {
-		// System.setProperty("phantomjs.binary.path",
-		// "/Users/Bingo/Downloads/phantomjs-1.9.7-macosx/bin/phantomjs");
-	}
-
-	/**
-	 * set sleep time to wait until load success
-	 *
-	 * @param sleepTime sleepTime
-	 * @return this
-	 */
-	public MySeleniumDownloader setSleepTime(int sleepTime) {
+	public MySeleniumDownloader(int sleepTime) {
 		this.sleepTime = sleepTime;
-		return this;
 	}
 
 	@Override
@@ -90,25 +58,16 @@ public class MySeleniumDownloader implements Downloader, Closeable {
 		WebDriver.Options manage = webDriver.manage();
 		Site site = task.getSite();
 		if (site.getCookies() != null) {
-			for (Map.Entry<String, String> cookieEntry : site.getCookies()
-					.entrySet()) {
-				Cookie cookie = new Cookie(cookieEntry.getKey(),
-						cookieEntry.getValue());
+			for (Map.Entry<String, String> cookieEntry : site.getCookies().entrySet()) {
+				Cookie cookie = new Cookie(cookieEntry.getKey(), cookieEntry.getValue());
 				manage.addCookie(cookie);
 			}
 		}
-
-		/*
-		 * TODO You can add mouse event or other processes
-		 * 
-		 * @author: bob.li.0718@gmail.com
-		 */
 
 		WebElement webElement = webDriver.findElement(By.xpath("/html"));
 		String content = webElement.getAttribute("outerHTML");
 		Page page = new Page();
 		page.setRawText(content);
-		// page.setHtml(new Html(content, request.getUrl()));
 		page.setUrl(new PlainText(request.getUrl()));
 		page.setRequest(request);
 		webDriverPool.returnToPool(webDriver);
@@ -118,7 +77,9 @@ public class MySeleniumDownloader implements Downloader, Closeable {
 	private void checkInit() {
 		if (webDriverPool == null) {
 			synchronized (this) {
-				webDriverPool = new WebDriverPool(poolSize);
+				if (webDriverPool == null) {
+					webDriverPool = new MyWebDriverPool(poolSize);
+				}
 			}
 		}
 	}
