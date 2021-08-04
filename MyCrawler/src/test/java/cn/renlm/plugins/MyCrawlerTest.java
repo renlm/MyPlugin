fbcc059603a0b2c6fb34b8d4f50cb4a478b51d0c
renlm <renlmer@qq.com>
2021-08-04 21:35:27 +0800

    sss

--- MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -45,11 +45,13 @@ public class MyCrawlerTest {
 					.get("根据法律、法规、规章和招标文件的规定,(.*?)（入场登记号：(\\w+)）已于(\\d{4}年\\d{1,2}月\\d{1,2}日)在", text, 2);
 			String openDate = ReUtil.get("根据法律、法规、规章和招标文件的规定,(.*?)（入场登记号：(\\w+)）已于(\\d{4}年\\d{1,2}月\\d{1,2}日)在", text,
 					3);
-			String bid1th = ReUtil.get("第一中标候选人(：|:)[^\\S\\r\\n]*([^\\s；，;,&nbsp;]*)", text, 2);
-			String bid1thPriceTitle = ReUtil.get("((投标价|投标报价)+(（元）)?)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 1);
-			String bid1thPriceValue = ReUtil.get("((投标价|投标报价)+(（元）)?)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 5);
-			String bid1thDurationTitle = ReUtil.get("(工期)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 1);
-			String bid1thDurationValue = ReUtil.get("(工期)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 3);
+			String bid1th = ReUtil.get("第一中标候选人[^：:]*(：|:)[^\\S\\r\\n]*([^\\s；，;,&nbsp;]*)", text, 2);
+			String bid1thPriceTitle = ReUtil.get("((投标价|投标报价|工程报价|下浮率)+[^：:]*)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text,
+					1);
+			String bid1thPriceValue = ReUtil.get("((投标价|投标报价|工程报价|下浮率)+[^：:]*)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text,
+					4);
+			String bid1thDurationTitle = ReUtil.get("((工期|监理服务期)[^：:]*)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 1);
+			String bid1thDurationValue = ReUtil.get("((工期|监理服务期)[^：:]*)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 4);
 			// 模板2：https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202107/t20210730_69347845.html
 			if (StrUtil.isBlank(projectName)) {
 				projectName = ReUtil.get(
@@ -106,6 +108,8 @@ public class MyCrawlerTest {
 		spider.addUrl("https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202107/t20210730_69347845.html");
 		spider.addUrl("https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202107/t20210730_69353450.html");
 		spider.addUrl("https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202107/t20210727_69299539.html");
+		spider.addUrl("https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202107/t20210715_69021375.html");
+		spider.addUrl("https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202108/t20210804_69404776.html");
 		spider.run();
 	}
 
