5295b5473fe7ef5d2768b663779048e71d8e62bd
renlm <renlmer@qq.com>
2021-08-04 20:58:10 +0800

    sss

--- MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -45,7 +45,7 @@ public class MyCrawlerTest {
 					.get("根据法律、法规、规章和招标文件的规定,(.*?)（入场登记号：(\\w+)）已于(\\d{4}年\\d{1,2}月\\d{1,2}日)在", text, 2);
 			String openDate = ReUtil.get("根据法律、法规、规章和招标文件的规定,(.*?)（入场登记号：(\\w+)）已于(\\d{4}年\\d{1,2}月\\d{1,2}日)在", text,
 					3);
-			String bid1th = ReUtil.get("第一中标候选人(：|:)[^\\S\\r\\n]*([^\\s；，;,&nbsp;]*)", text, 1);
+			String bid1th = ReUtil.get("第一中标候选人(：|:)[^\\S\\r\\n]*([^\\s；，;,&nbsp;]*)", text, 2);
 			String bid1thPriceTitle = ReUtil.get("((投标价|投标报价)+(（元）)?)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 1);
 			String bid1thPriceValue = ReUtil.get("((投标价|投标报价)+(（元）)?)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 5);
 			String bid1thDurationTitle = ReUtil.get("(工期)(：|:)[^\\S\\r\\n]*([^\\s；，;,]*)", text, 1);
@@ -64,7 +64,6 @@ public class MyCrawlerTest {
 				openDate = ReUtil.get(
 						"根据法律、法规、规章和招标文件的规定，(.*?)（招标人名称）的(.*?)（项目名称），\\(入场登记号：(\\w+)\\)已于(\\d{4}年\\d{1,2}月\\d{1,2}日)在",
 						text, 4);
-				bid1th = ReUtil.get("第一中标候选人:([^\\s；&nbsp;]*)", text, 1);
 			}
 			// 模板3：https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202107/t20210730_69353450.html
 			if (StrUtil.isBlank(projectName)) {
@@ -80,7 +79,6 @@ public class MyCrawlerTest {
 				openDate = ReUtil.get(
 						"根据法律、法规、规章和招标文件的规定, (.*?)（招标人名称）的(.*?)（项目名称）(\\w+) \\(入场登记号：\\)已于(\\d{4}年\\d{1,2}月\\d{1,2}日)在",
 						text, 4);
-				bid1th = ReUtil.get("第一中标候选人: ([^\\s]*)", text, 1);
 			}
 			// 模板4：https://www.bijie.gov.cn/bm/bjsggzyjyzx/jy/jsgc/zbgs/202107/t20210727_69299539.html
 			if (StrUtil.isBlank(projectName)) {
@@ -93,7 +91,6 @@ public class MyCrawlerTest {
 				openDate = ReUtil.get(
 						"根据法律、法规、规章和招标文件的规定，(.*?)（项目编号为：(\\w+)），已于(\\d{4}年\\d{1,2}月\\d{1,2}日\\d{1,2}时\\d{1,2}分)在", text,
 						3);
-				bid1th = ReUtil.get("第一中标候选人：([^\\s]*)", text, 1);
 			}
 			System.out.println("====== " + projectName);
 			System.out.println("====== " + company);
