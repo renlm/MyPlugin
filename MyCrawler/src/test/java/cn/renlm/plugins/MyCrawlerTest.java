ed70ed2a142f9c085963c2050b3377874e0f2ab6
renlm <renlmer@qq.com>
2021-08-06 21:29:58 +0800

    sss

--- MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
+++ MyCrawler/src/test/java/cn/renlm/plugins/MyCrawlerTest.java
@@ -1,12 +1,22 @@
 package cn.renlm.plugins;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HtmlUtil;
 import cn.hutool.setting.Setting;
 import cn.renlm.plugins.MyCrawler.MySite;
 import cn.renlm.plugins.MyCrawler.MySpider;
+import cn.renlm.plugins.MyCrawler.PageUrlType;
 import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.selector.Html;
 
 /**
  * 爬虫测试
@@ -19,16 +29,74 @@ public class MyCrawlerTest {
 	String dataUrlRegex = ResourceUtil.readUtf8Str("dataUrl.regex");
 	Setting chromeSetting = new Setting("config/chrome.setting");
 
+	/**
+	 * 提取字段
+	 */
 	@Test
 	public void fetchField() {
 		MySite site = MySite.me();
 		site.setEnableSelenuim(false);
 		site.setChromeSetting(chromeSetting);
+		site.setSleepTime(0);
 		MySpider spider = MyCrawlerUtil.createSpider(site, myPage -> {
 			Page page = myPage.page();
-			System.out.println(page.getHtml().xpath("//div[@class='book-info']/h1/em/text()").get());
+			Html html = page.getHtml();
+
+			List<List<String>> rows = new ArrayList<>();
+			html.xpath("//font[@id='Zoom']/div/table/tbody/tr").nodes().forEach(tr -> {
+				List<String> cols = new ArrayList<>();
+				rows.add(cols);
+				tr.xpath("/tr/td").nodes().forEach(td -> {
+					String text = StrUtil.trim(HtmlUtil.cleanHtmlTag(td.get()));
+					cols.add(ReUtil.delAll("\\s", ReUtil.delAll("\\p{Z}", text)));
+				});
+				if (rows.size() == 2) {
+					Map<String, String> data = CollUtil.zip(rows.get(0), cols);
+					data.forEach((k, v) -> {
+						if (StrUtil.containsAny(k, "成交价", "成交总价")) {
+							System.out.println(k + "：" + v);
+						}
+						if (StrUtil.containsAny(k, "受让人名称", "竞得单位名称", "竞得人")) {
+							System.out.println(k + "：" + v);
+						}
+					});
+				}
+			});
 		});
-		spider.addUrl("https://book.qidian.com/info/107580");
+		spider.addUrl("http://ggzy.guiyang.gov.cn/tdcr/cjjggs/xfx_5372564/201302/t20130219_61321885.html");
+		spider.addUrl("http://ggzy.guiyang.gov.cn/tdcr/cjjggs/nmq_5372559/201304/t20130403_61321892.html");
+		spider.addUrl("http://ggzy.guiyang.gov.cn/tdcr/cjjggs/nmq_5372559/202009/t20200903_62859936.html");
+		spider.run();
+	}
+
+	/**
+	 * 查找链接
+	 */
+	@Test
+	public void findUrls() {
+		MySite site = MySite.me();
+		site.setEnableSelenuim(true);
+		site.setChromeSetting(chromeSetting);
+		MySpider spider = MyCrawlerUtil.createSpider(site, myPage -> {
+			Page page = myPage.page();
+			System.out.println();
+			System.out.println();
+			List<String> seedUrls = CollUtil
+					.removeBlank(CollUtil.distinct(page.getHtml().links().regex(seedUrlRegex, 0).all()));
+			seedUrls.forEach(url -> {
+				System.out.println("=== SEED === " + PageUrlType.standardUrl(url, false, "v"));
+			});
+			System.out.println();
+			System.out.println();
+			List<String> dataUrls = CollUtil
+					.removeBlank(CollUtil.distinct(page.getHtml().links().regex(dataUrlRegex, 0).all()));
+			dataUrls.forEach(url -> {
+				System.out.println("=== DATA === " + PageUrlType.standardUrl(url, false, "v"));
+			});
+			System.out.println();
+			System.out.println();
+		});
+		spider.addUrl("http://ggzy.guiyang.gov.cn/gcjs/zbhxrgs/jl_5372461/index.html?i=9");
 		spider.run();
 	}
 }
