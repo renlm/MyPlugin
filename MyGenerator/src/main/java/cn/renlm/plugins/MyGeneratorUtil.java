61adff4ff4928e01f29198ea20579639bf162a6c
renlm <renlmer@163.com>
2022-01-03 18:45:49 +0800

    ssx

--- MyGenerator/src/main/java/cn/renlm/plugins/MyGeneratorUtil.java
+++ MyGenerator/src/main/java/cn/renlm/plugins/MyGeneratorUtil.java
@@ -1,54 +1,45 @@
 package cn.renlm.plugins;
 
-import static com.baomidou.mybatisplus.core.toolkit.StringPool.DOT_XML;
 import static com.baomidou.mybatisplus.core.toolkit.StringPool.SLASH;
 
-import java.io.File;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
-import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
-import com.baomidou.mybatisplus.generator.config.IFileCreate;
+import com.baomidou.mybatisplus.generator.config.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.TemplateConfig;
-import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
-import com.baomidou.mybatisplus.generator.config.converts.PostgreSqlTypeConvert;
-import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
-import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
-import com.baomidou.mybatisplus.generator.config.rules.FileType;
-import com.baomidou.mybatisplus.generator.config.rules.IColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import com.thoughtworks.xstream.annotations.XStreamImplicit;
 
 import cn.hutool.core.util.ClassUtil;
+import cn.hutool.core.util.ObjectUtil;
 import cn.renlm.plugins.MyUtil.MyXStreamUtil;
 import lombok.Data;
 
 /**
  * 代码生成封装类
  * 	spring-boot
- * 		2.3.2.RELEASE
- * 	mybatis-plus-boot-starter
- * 	mybatis-plus-generator
- * 		3.4.1
- *  dynamic-datasource-spring-boot-starter
- *  	3.3.1
- *  freemarker
- *  	2.3.30
+ * 		2.5.3 
+ *  mybatis-plus-boot-starter 
+ *  	3.4.3.4
+ *  dynamic-datasource-spring-boot-starter 
+ *  	3.5.0 
+ *  mybatis-plus-generator 
+ *  	3.5.1
+ *  freemarker 
+ *  	2.3.31
  * 
  * @author Renlm
  *
@@ -58,10 +49,9 @@ public class MyGeneratorUtil {
 	static final String excelXmlSuffix 			= ".excel.xml";
 	static final String mapperOutputDir 		= ConstVal.resourcesDir + "/mapper";
 	static final String excelXmlOutputDir 		= ConstVal.resourcesDir + "/excel";
-	static final String mapperTemplatePath		= "/templates/mapper.xml.ftl";
-	static final String excelXmlTemplatePath	= "config/Excel.xml.ftl";
-	static final String serviceImplTemplatePath	= "config/ServiceImpl.java";
-	static final String dSClassName 			= "com.baomidou.dynamic.datasource.annotation.DS";
+	static final String excelXmlTemplatePath 	= "config/Excel.xml.ftl";
+	static final String serviceImplTemplatePath = "config/ServiceImpl.java";
+	static final String dSClassName 			= DS.class.getName();
 
 	/**
 	 * 读取配置并运行
@@ -70,24 +60,7 @@ public class MyGeneratorUtil {
 	 */
 	public static final void run(String xml) {
 		GeneratorConfig conf = MyXStreamUtil.read(GeneratorConfig.class, xml);
-		DataSourceConfig dsc = new DataSourceConfig()
-				.setUrl(conf.url)
-				.setUsername(conf.username)
-				.setPassword(conf.password)
-				.setDriverName(conf.driverName);
-		DbType dbType = dsc.getDbType();
-		if (dbType == DbType.POSTGRE_SQL) {
-			dsc.setTypeConvert(new PostgreSqlTypeConvert() {
-				@Override
-				public IColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
-					if (fieldType.toLowerCase().contains("bytea")) {
-						return DbColumnType.BYTE_ARRAY;
-					}
-					return super.processTypeConvert(globalConfig, fieldType);
-				}
-
-			});
-		}
+		DataSourceConfig dsc = new DataSourceConfig.Builder(conf.url, conf.username, conf.password).build();
 		conf.modules.forEach(module -> {
 			module.tables.forEach(table -> {
 				create(conf, dsc, module.pkg, module.name, table);
@@ -97,103 +70,91 @@ public class MyGeneratorUtil {
 
 	private static final void create(GeneratorConfig conf, DataSourceConfig dsc, String pkg, String moduleName,
 			GeneratorTable table) {
-		AutoGenerator mpg = new AutoGenerator();
-		mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+		AutoGenerator autoGenerator = new AutoGenerator(dsc);
+		autoGenerator.injection(injectionConfig(conf));
+		autoGenerator.template(templateConfig());
+		autoGenerator.strategy(strategyConfig(table));
+		autoGenerator.packageInfo(packageConfig(pkg, moduleName));
+		autoGenerator.global(globalConfig(table));
+		autoGenerator.execute();
 
-		// 全局配置
-		GlobalConfig gc = new GlobalConfig();
-		gc.setOutputDir(ConstVal.javaDir);
-		gc.setAuthor(table.author);
-		gc.setOpen(false);
-		gc.setIdType(table.idType == null ? IdType.AUTO : IdType.valueOf(table.idType));
-		gc.setDateType(DateType.ONLY_DATE);
-		mpg.setGlobalConfig(gc);
+	}
 
-		// 数据源配置
-		mpg.setDataSource(dsc);
+	/**
+	 * 注入配置
+	 * 
+	 * @param conf
+	 * @return
+	 */
+	private static final InjectionConfig injectionConfig(GeneratorConfig conf) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("nameOfDS", ClassUtil.loadClass(dSClassName).getName());
+		map.put("dsName", conf.dsName);
+		
+		Map<String, String> customFile = new HashMap<>();
+		customFile.put(excelXmlSuffix, excelXmlTemplatePath);
+		
+		return new InjectionConfig.Builder()
+				.customMap(map)
+				.customFile(customFile)
+				.build();
+	}
 
-		// 包配置
-		PackageConfig pc = new PackageConfig();
-		pc.setModuleName(moduleName);
-		pc.setParent(pkg);
-		mpg.setPackageInfo(pc);
+	/**
+	 * 模板配置
+	 */
+	private static final TemplateConfig templateConfig() {
+		return new TemplateConfig.Builder()
+				.serviceImpl(serviceImplTemplatePath)
+				.controller(null)
+				.build();
+	}
 
-		// 自定义配置
-		InjectionConfig cfg = new InjectionConfig() {
-			@Override
-			public void initMap() {
-				Map<String, Object> map = new HashMap<String, Object>();
-				Class<?> dsClass = null;
-				try {
-					dsClass = ClassUtil.loadClass(dSClassName);
-					map.put("nameOfDS", dsClass.getName());
-					map.put("dsName", conf.dsName);
-				} catch(Exception e) {}
-				this.setMap(map);
-			}
-		};
+	/**
+	 * 策略配置
+	 */
+	private static final StrategyConfig strategyConfig(GeneratorTable table) {
+		return new StrategyConfig.Builder()
+				.addInclude(table.name)
+				.entityBuilder()
+				.idType(ObjectUtil.defaultIfNull(IdType.AUTO, IdType.valueOf(table.idType)))
+				.enableTableFieldAnnotation()
+				.enableChainModel()
+				.naming(NamingStrategy.underline_to_camel)
+				.columnNaming(NamingStrategy.underline_to_camel)
+				.build();
+	}
 
-		// 自定义输出配置
-		List<FileOutConfig> focList = new ArrayList<>();
-		// 自定义配置会被优先输出
-		focList.add(new FileOutConfig(mapperTemplatePath) {
-			@Override
-			public String outputFile(TableInfo tableInfo) {
-				// 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-				return mapperOutputDir + SLASH 
-						+ pc.getModuleName() + SLASH 
-						+ tableInfo.getEntityName() + mapperSuffix + DOT_XML;
-			}
-		});
-		focList.add(new FileOutConfig(excelXmlTemplatePath) {
-			@Override
-			public String outputFile(TableInfo tableInfo) {
-				return excelXmlOutputDir + SLASH 
-						+ pc.getModuleName() + SLASH 
-						+ tableInfo.getEntityName() + excelXmlSuffix;
-			}
-		});
-		cfg.setFileCreate(new IFileCreate() {
-			@Override
-			public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-				if (filePath.endsWith(excelXmlSuffix)
-						&& !table.configExcel) {
-					return false;
-				}
-				this.checkDir(filePath);
-				return (fileType == FileType.ENTITY && table.coverEntity) 
-						|| !new File(filePath).exists();
-			}
+	/**
+	 * 包配置
+	 * 
+	 * @param pkg
+	 * @param moduleName
+	 * @return
+	 */
+	private static final PackageConfig packageConfig(String pkg, String moduleName) {
+		Map<OutputFile, String> pathInfo = new HashMap<>();
+		pathInfo.put(OutputFile.mapperXml, mapperOutputDir + SLASH + moduleName + SLASH);
+		return new PackageConfig.Builder()
+				.parent(pkg)
+				.moduleName(moduleName)
+				.pathInfo(pathInfo)
+				.build();
+	}
 
-			@Override
-			public void checkDir(String filePath) {
-				File file = new File(filePath);
-				boolean exist = file.exists();
-				if (!exist) {
-					file.getParentFile().mkdirs();
-				}
-			}
-		});
-		cfg.setFileOutConfigList(focList);
-		mpg.setCfg(cfg);
-
-		// 配置模板
-		TemplateConfig templateConfig = new TemplateConfig();
-
-		templateConfig.setXml(null);
-		templateConfig.setServiceImpl(serviceImplTemplatePath);
-		templateConfig.setController(null);
-		mpg.setTemplate(templateConfig);
-
-		// 策略配置
-		StrategyConfig strategy = new StrategyConfig();
-		strategy.setNaming(NamingStrategy.underline_to_camel);
-		strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-		strategy.setEntityLombokModel(true);
-		strategy.setEntityTableFieldAnnotationEnable(true);
-		strategy.setInclude(table.name);
-		mpg.setStrategy(strategy);
-		mpg.execute();
+	/**
+	 * 全局配置
+	 * 
+	 * @param table
+	 * @return
+	 */
+	private static final GlobalConfig globalConfig(GeneratorTable table) {
+		return new GlobalConfig.Builder()
+				.outputDir(ConstVal.javaDir)
+				.author(table.author)
+				.disableOpenDir()
+				.dateType(DateType.ONLY_DATE)
+				.build();
 	}
 
 	/**
