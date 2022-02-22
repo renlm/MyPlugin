787f890907568f31a702448609cc9a917ae62dc6
renlm <renlmer@163.com>
2022-02-22 16:07:54 +0800

    sss

---
+++ MyUtil/src/main/java/cn/renlm/plugins/MyUtil/MyDbUtil.java
@@ -0,0 +1,51 @@
+package cn.renlm.plugins.MyUtil;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.db.ds.DSFactory;
+import cn.hutool.db.meta.MetaUtil;
+import cn.hutool.db.meta.Table;
+import cn.hutool.db.meta.TableType;
+import cn.hutool.setting.Setting;
+import lombok.experimental.UtilityClass;
+
+/**
+ * 数据库操作工具
+ * 
+ * @author Renlm
+ *
+ */
+@UtilityClass
+public class MyDbUtil {
+
+	/**
+	 * 获得表的元信息
+	 * 
+	 * @param schema
+	 * @param jdbcUrl
+	 * @param username
+	 * @param password
+	 * @param types
+	 * @return
+	 */
+	public List<Table> getTableMetas(String schema, String jdbcUrl, String username, String password,
+			TableType... types) {
+		Setting setting = new Setting();
+		setting.set(DSFactory.KEY_ALIAS_URL[0], jdbcUrl);
+		setting.set(DSFactory.KEY_ALIAS_USER[0], username);
+		setting.set(DSFactory.KEY_ALIAS_PASSWORD[0], password);
+		setting.set(DSFactory.KEY_CONN_PROPS[0], String.valueOf(true));
+		setting.set(DSFactory.KEY_CONN_PROPS[1], String.valueOf(true));
+		DSFactory dsf = DSFactory.create(setting);
+		DataSource ds = dsf.getDataSource();
+		List<Table> tables = CollUtil.newArrayList();
+		MetaUtil.getTables(dsf.getDataSource(), schema, types).forEach(tableName -> {
+			Table table = MetaUtil.getTableMeta(ds, tableName);
+			tables.add(table);
+		});
+		return tables;
+	}
+}
