a32f481abe901f72ce08cdda3049fa570c14aa8f
renlm <renlmer@163.com>
2022-03-04 10:49:29 +0800

    升级

--- MyUtil/src/main/java/cn/renlm/plugins/MyUtil/MyDbUtil.java
+++ MyUtil/src/main/java/cn/renlm/plugins/MyUtil/MyDbUtil.java
@@ -30,8 +30,8 @@ public class MyDbUtil {
 	 * @param types
 	 * @return
 	 */
-	public static final List<Table> getTableMetas(String jdbcUrl, String username, String password,
-			TableType... types) {
+	public static final List<Table> getTableMetas(String jdbcUrl,
+			String username, String password, TableType... types) {
 		Setting setting = new Setting();
 		setting.set(DSFactory.KEY_ALIAS_URL[0], jdbcUrl);
 		setting.set(DSFactory.KEY_ALIAS_USER[0], username);
@@ -45,6 +45,36 @@ public class MyDbUtil {
 			Table table = MetaUtil.getTableMeta(ds, tableName);
 			tables.add(table);
 		});
+		return tables;
+	}
+
+	/**
+	 * 获得表的元信息
+	 * 
+	 * @param jdbcUrl
+	 * @param schema
+	 * @param username
+	 * @param password
+	 * @param types
+	 * @return
+	 */
+	public static final List<Table> getTableMetas(String jdbcUrl, String schema,
+			String username, String password, TableType... types) {
+		Setting setting = new Setting();
+		setting.set(DSFactory.KEY_ALIAS_URL[0], jdbcUrl);
+		setting.set(DSFactory.KEY_ALIAS_USER[0], username);
+		setting.set(DSFactory.KEY_ALIAS_PASSWORD[0], password);
+		setting.set(DSFactory.KEY_CONN_PROPS[0], String.valueOf(true));
+		setting.set(DSFactory.KEY_CONN_PROPS[1], String.valueOf(true));
+		DSFactory dsf = DSFactory.create(setting);
+		DataSource ds = dsf.getDataSource();
+		List<Table> tables = CollUtil.newArrayList();
+		MetaUtil.getTables(dsf.getDataSource(), schema, types)
+				.forEach(tableName -> {
+					Table table = MetaUtil.getTableMeta(ds, null, schema,
+							tableName);
+					tables.add(table);
+				});
 		return tables;
 	}
 }
