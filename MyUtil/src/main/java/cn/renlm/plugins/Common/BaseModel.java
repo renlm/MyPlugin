1673d1ef411e1e45c9f1c55440fdfd5bc717b1d2
任黎明(10021235) <renlm-a@glodon.com>
2023-02-18 21:17:16 +0800

    up

--- MyUtil/src/main/java/cn/renlm/plugins/Common/BaseModel.java
+++ MyUtil/src/main/java/cn/renlm/plugins/Common/BaseModel.java
@@ -3,6 +3,10 @@ package cn.renlm.plugins.Common;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+
 import lombok.Data;
 
 /**
@@ -19,16 +23,19 @@ public class BaseModel implements Serializable {
 	/**
 	 * 创建时间
 	 */
+	@TableField(value = "created_at", fill = FieldFill.INSERT)
 	private Date createdAt;
 
 	/**
 	 * 更新时间
 	 */
+	@TableField(value = "updated_at", fill = FieldFill.UPDATE)
 	private Date updatedAt;
 
 	/**
 	 * 是否删除
 	 */
+	@TableLogic(value = "false", delval = "true")
 	private Boolean deleted;
 
 }
