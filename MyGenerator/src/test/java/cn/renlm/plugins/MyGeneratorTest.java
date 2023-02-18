a265de5381f98e29d7bdcc125bf9a7ab8d9cbe12
任黎明(10021235) <renlm-a@glodon.com>
2023-02-18 20:03:03 +0800

    up

--- MyGenerator/src/test/java/cn/renlm/plugins/MyGeneratorTest.java
+++ MyGenerator/src/test/java/cn/renlm/plugins/MyGeneratorTest.java
@@ -2,10 +2,6 @@ package cn.renlm.plugins;
 
 import org.junit.jupiter.api.Test;
 
-import cn.hutool.json.JSONUtil;
-import cn.renlm.plugins.MyGeneratorUtil.GeneratorConfig;
-import cn.renlm.plugins.MyUtil.MyXStreamUtil;
-
 /**
  * 代码生成封装
  * 
@@ -17,12 +13,6 @@ public class MyGeneratorTest {
 	@Test
 	public void run() {
 		MyGeneratorUtil.run("MyGenerator.xml");
-	}
-
-	@Test
-	public void read() {
-		GeneratorConfig conf = MyXStreamUtil.read(GeneratorConfig.class, "MyGenerator.xml");
-		System.out.println(JSONUtil.toJsonPrettyStr(conf));
 	}
 
 }
