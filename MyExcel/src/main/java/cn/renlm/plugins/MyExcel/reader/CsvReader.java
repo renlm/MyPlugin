ae50add29c6623a0f53acb370a9a5877cb503155
renlm-a <renlm-a@glodon.com>
2023-03-04 21:47:16 +0800

    poi

--- MyExcel/src/main/java/cn/renlm/plugins/MyExcel/reader/CsvReader.java
+++ MyExcel/src/main/java/cn/renlm/plugins/MyExcel/reader/CsvReader.java
@@ -2,11 +2,13 @@ package cn.renlm.plugins.MyExcel.reader;
 
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.text.csv.CsvUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.renlm.plugins.MyExcel.config.MySheet;
 import cn.renlm.plugins.MyExcel.config.MyWorkbook;
 import cn.renlm.plugins.MyExcel.handler.DataReadHandler;
@@ -25,13 +27,17 @@ public class CsvReader extends AbstractReader {
 
 	@Override
 	public AbstractReader read(String sheetName, DataReadHandler dataReadHandler) {
-		final MySheet sheet = myExcel.getSheetByName(sheetName);
-
 		final List<List<String>> titles = new ArrayList<>();
 		final List<String> keys = new ArrayList<>();
 
+		final MySheet sheet = myExcel.getSheetByName(sheetName);
+		Charset charset = StandardCharsets.UTF_8;
+		if (StrUtil.isNotBlank(myExcel.getCsvCharset())) {
+			charset = Charset.forName(myExcel.getCsvCharset());
+		}
+
 		cn.hutool.core.text.csv.CsvReader reader = CsvUtil.getReader();
-		reader.read(IoUtil.getReader(in, Charset.forName(myExcel.getCsvCharset())), csvRow -> {
+		reader.read(IoUtil.getReader(in, charset), csvRow -> {
 			Long rowIndex = csvRow.getOriginalLineNumber();
 			List<Object> rowList = new ArrayList<>(csvRow.getRawList());
 			super.processRow(myExcel, titles, keys, dataReadHandler, sheet, rowIndex, rowList);
