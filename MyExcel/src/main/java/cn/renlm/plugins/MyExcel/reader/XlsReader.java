14279bd637c7c470694ad2118e5224e61673413a
renlm-a <renlm-a@glodon.com>
2023-03-05 18:05:21 +0800

    poi

--- MyExcel/src/main/java/cn/renlm/plugins/MyExcel/reader/XlsReader.java
+++ MyExcel/src/main/java/cn/renlm/plugins/MyExcel/reader/XlsReader.java
@@ -11,6 +11,8 @@ import org.apache.poi.hssf.eventusermodel.HSSFEventFactory;
 import org.apache.poi.hssf.eventusermodel.HSSFListener;
 import org.apache.poi.hssf.eventusermodel.HSSFRequest;
 import org.apache.poi.hssf.eventusermodel.MissingRecordAwareHSSFListener;
+import org.apache.poi.hssf.eventusermodel.dummyrecord.LastCellOfRowDummyRecord;
+import org.apache.poi.hssf.eventusermodel.dummyrecord.MissingCellDummyRecord;
 import org.apache.poi.hssf.model.HSSFFormulaParser;
 import org.apache.poi.hssf.record.BOFRecord;
 import org.apache.poi.hssf.record.BoolErrRecord;
@@ -25,6 +27,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.poi.excel.sax.ExcelSaxUtil;
 import cn.hutool.poi.exceptions.POIException;
@@ -42,30 +45,18 @@ import lombok.SneakyThrows;
 public class XlsReader extends AbstractReader implements HSSFListener {
 
 	private MySheet mySheet;
+	private DataReadHandler dataReadHandler;
 
 	private FormatTrackingHSSFListener formatListener;
-
 	private SheetRecordCollectingListener workbookBuildingListener;
-
 	private SSTRecord sstRecord;
-
 	private HSSFWorkbook stubWorkbook;
-
 	private final List<BoundSheetRecord> boundSheetRecords = new ArrayList<>();
 
-	/**
-	 * 自定义需要处理的sheet编号
-	 */
 	private Integer rSheetIndex = -1;
-
-	/**
-	 * 当前sheet编号
-	 */
 	private Integer sheetIndex = -1;
-
-	/**
-	 * 行数据
-	 */
+	final List<List<String>> titles = new ArrayList<>();
+	final List<String> keys = new ArrayList<>();
 	private List<Object> rowCells = new ArrayList<>();
 
 	public XlsReader(MyWorkbook myExcel, InputStream in) {
@@ -78,8 +69,16 @@ public class XlsReader extends AbstractReader implements HSSFListener {
 		if (in.markSupported()) {
 			in.reset();
 		}
-
-		mySheet = myExcel.getSheetByName(sheetName);
+		this.sstRecord = null;
+		this.stubWorkbook = null;
+		this.boundSheetRecords.clear();
+		this.rSheetIndex = 0;
+		this.sheetIndex = -1;
+		this.titles.clear();
+		this.keys.clear();
+		this.rowCells.clear();
+		this.mySheet = myExcel.getSheetByName(sheetName);
+		this.dataReadHandler = dataReadHandler;
 		try (POIFSFileSystem fs = new POIFSFileSystem(in)) {
 			formatListener = new FormatTrackingHSSFListener(new MissingRecordAwareHSSFListener(this));
 			final HSSFRequest request = new HSSFRequest();
@@ -108,9 +107,10 @@ public class XlsReader extends AbstractReader implements HSSFListener {
 		}
 
 		Object value = null;
-		switch (record.getSid()) {
+		final short sid = record.getSid();
+		switch (sid) {
 		case BoundSheetRecord.sid:
-			BoundSheetRecord boundSheetRecord = (BoundSheetRecord) record;
+			final BoundSheetRecord boundSheetRecord = (BoundSheetRecord) record;
 			this.boundSheetRecords.add(boundSheetRecord);
 			String currentSheetName = boundSheetRecord.getSheetname();
 			if (this.rSheetIndex < 0 && StrUtil.equals(this.mySheet.getName(), currentSheetName)) {
@@ -118,7 +118,7 @@ public class XlsReader extends AbstractReader implements HSSFListener {
 			}
 			break;
 		case BOFRecord.sid:
-			BOFRecord bofRecord = (BOFRecord) record;
+			final BOFRecord bofRecord = (BOFRecord) record;
 			if (bofRecord.getType() == BOFRecord.TYPE_WORKSHEET) {
 				this.sheetIndex++;
 				if (this.workbookBuildingListener != null && this.stubWorkbook == null) {
@@ -129,32 +129,50 @@ public class XlsReader extends AbstractReader implements HSSFListener {
 		case SSTRecord.sid:
 			this.sstRecord = (SSTRecord) record;
 			break;
-		case BoolErrRecord.sid:
-			BoolErrRecord berec = (BoolErrRecord) record;
-			value = berec.getBooleanValue();
-			this.addToRowCells(berec.getRow(), berec.getColumn(), value);
-			break;
-		case FormulaRecord.sid:
-			FormulaRecord formulaRec = (FormulaRecord) record;
-			value = HSSFFormulaParser.toFormulaString(this.stubWorkbook, formulaRec.getParsedExpression());
-			this.addToRowCells(formulaRec.getRow(), formulaRec.getColumn(), value);
-			break;
-		case LabelRecord.sid:
-			LabelRecord lrec = (LabelRecord) record;
-			this.addToRowCells(lrec.getRow(), lrec.getColumn(), lrec.getValue());
-			break;
-		case LabelSSTRecord.sid:
-			LabelSSTRecord lsrec = (LabelSSTRecord) record;
-			value = this.sstRecord == null ? null : this.sstRecord.getString(lsrec.getSSTIndex()).toString();
-			this.addToRowCells(lsrec.getRow(), lsrec.getColumn(), value);
-			break;
-		case NumberRecord.sid:
-			NumberRecord numrec = (NumberRecord) record;
-			value = ExcelSaxUtil.getNumberOrDateValue(numrec, numrec.getValue(), this.formatListener);
-			this.addToRowCells(numrec.getRow(), numrec.getColumn(), value);
-			break;
 		default:
 			break;
+		}
+
+		if (NumberUtil.equals(this.rSheetIndex, this.sheetIndex)) {
+			switch (sid) {
+			case BoolErrRecord.sid:
+				final BoolErrRecord berec = (BoolErrRecord) record;
+				value = berec.getBooleanValue();
+				this.addToRowCells(berec.getRow(), berec.getColumn(), value);
+				break;
+			case FormulaRecord.sid:
+				final FormulaRecord formulaRec = (FormulaRecord) record;
+				value = HSSFFormulaParser.toFormulaString(this.stubWorkbook, formulaRec.getParsedExpression());
+				this.addToRowCells(formulaRec.getRow(), formulaRec.getColumn(), value);
+				break;
+			case LabelRecord.sid:
+				final LabelRecord lrec = (LabelRecord) record;
+				this.addToRowCells(lrec.getRow(), lrec.getColumn(), lrec.getValue());
+				break;
+			case LabelSSTRecord.sid:
+				final LabelSSTRecord lsrec = (LabelSSTRecord) record;
+				value = this.sstRecord == null ? null : this.sstRecord.getString(lsrec.getSSTIndex()).toString();
+				this.addToRowCells(lsrec.getRow(), lsrec.getColumn(), value);
+				break;
+			case NumberRecord.sid:
+				final NumberRecord numrec = (NumberRecord) record;
+				value = ExcelSaxUtil.getNumberOrDateValue(numrec, numrec.getValue(), this.formatListener);
+				this.addToRowCells(numrec.getRow(), numrec.getColumn(), value);
+				break;
+			default:
+				break;
+			}
+
+			if (record instanceof MissingCellDummyRecord) {
+				MissingCellDummyRecord mcdrec = (MissingCellDummyRecord) record;
+				this.addToRowCells(mcdrec.getRow(), mcdrec.getColumn(), null);
+			}
+
+			if (record instanceof LastCellOfRowDummyRecord) {
+				LastCellOfRowDummyRecord lastCell = (LastCellOfRowDummyRecord) record;
+				super.processRow(myExcel, titles, keys, dataReadHandler, mySheet, lastCell.getRow(), rowCells);
+				this.rowCells = new ArrayList<>(this.rowCells.size());
+			}
 		}
 	}
 
