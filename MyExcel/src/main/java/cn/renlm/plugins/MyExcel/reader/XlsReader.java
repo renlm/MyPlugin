0cd0f2e17c29383002dd134376a3b4b89814365e
renlm-a <renlm-a@glodon.com>
2023-03-05 15:30:48 +0800

    poi

--- MyExcel/src/main/java/cn/renlm/plugins/MyExcel/reader/XlsReader.java
+++ MyExcel/src/main/java/cn/renlm/plugins/MyExcel/reader/XlsReader.java
@@ -1,10 +1,19 @@
 package cn.renlm.plugins.MyExcel.reader;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.poi.hssf.eventusermodel.EventWorkbookBuilder.SheetRecordCollectingListener;
+import org.apache.poi.hssf.eventusermodel.FormatTrackingHSSFListener;
+import org.apache.poi.hssf.eventusermodel.HSSFEventFactory;
 import org.apache.poi.hssf.eventusermodel.HSSFListener;
+import org.apache.poi.hssf.eventusermodel.HSSFRequest;
+import org.apache.poi.hssf.eventusermodel.MissingRecordAwareHSSFListener;
 import org.apache.poi.hssf.record.Record;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.poi.exceptions.POIException;
 import cn.renlm.plugins.MyExcel.config.MyWorkbook;
 import cn.renlm.plugins.MyExcel.handler.DataReadHandler;
 import lombok.SneakyThrows;
@@ -17,6 +26,10 @@ import lombok.SneakyThrows;
  */
 public class XlsReader extends AbstractReader implements HSSFListener {
 
+	private FormatTrackingHSSFListener formatListener;
+
+	private SheetRecordCollectingListener workbookBuildingListener;
+
 	public XlsReader(MyWorkbook myExcel, InputStream in) {
 		super(myExcel, in);
 	}
@@ -26,6 +39,24 @@ public class XlsReader extends AbstractReader implements HSSFListener {
 	public AbstractReader read(String sheetName, DataReadHandler dataReadHandler) {
 		if (in.markSupported()) {
 			in.reset();
+		}
+
+		try (POIFSFileSystem fs = new POIFSFileSystem(in)) {
+			formatListener = new FormatTrackingHSSFListener(new MissingRecordAwareHSSFListener(this));
+			final HSSFRequest request = new HSSFRequest();
+			workbookBuildingListener = new SheetRecordCollectingListener(formatListener);
+			request.addListenerForAllRecords(workbookBuildingListener);
+
+			final HSSFEventFactory factory = new HSSFEventFactory();
+			try {
+				factory.processWorkbookEvents(request, fs);
+			} catch (IOException e) {
+				throw new POIException(e);
+			} finally {
+				IoUtil.close(fs);
+			}
+		} catch (IOException e) {
+			throw new POIException(e);
 		}
 
 		return this;
