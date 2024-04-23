5301a6cf424d4d69d8f312e60117e81835901a57
renlm-a <renlm-a@glodon.com>
2024-04-23 10:05:44 +0800

    up

--- MyGenerator/src/test/java/cn/renlm/plugins/MyGeneratorTest.java
+++ MyGenerator/src/test/java/cn/renlm/plugins/MyGeneratorTest.java
@@ -1,6 +1,11 @@
 package cn.renlm.plugins;
 
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.Test;
+
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
 
 /**
  * 代码生成封装
@@ -13,6 +18,15 @@ public class MyGeneratorTest {
 	@Test
 	public void run() {
 		MyGeneratorUtil.run("MyGenerator.xml");
+	}
+
+	@Test
+	public void javaSqlTypes() {
+		Field[] fields = ReflectUtil.getFields(java.sql.Types.class);
+		for (Field field : fields) {
+			String str = StrUtil.format("<xsd:enumeration value=\"{}\"/>", field.getName());
+			System.out.println(str);
+		}
 	}
 
 }
